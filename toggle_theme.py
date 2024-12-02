4ba938d912ba1b143b37f9374be86251088902d7
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2024-12-02 21:53:35 +0000

    Update toggle_theme.py

--- toggle_theme.py
+++ toggle_theme.py
@@ -1,13 +1,18 @@
-
 import streamlit as st
 
 def toggle_theme():
+    if 'theme' not in st.session_state:
+        st.session_state.theme = 'Light'
     theme = st.radio("Select Theme:", ["Light", "Dark"], key='theme_toggle')
-    if theme == "Dark":
+    if theme != st.session_state.theme:
+        st.session_state.theme = theme
+        st.experimental_rerun()
+
+    if st.session_state.theme == "Dark":
         st.markdown(
             """
             <style>
-            html, body, [class*="css"]  {
+            .stApp {
                 background-color: #0E1117 !important;
                 color: #FFFFFF !important;
             }
@@ -19,7 +24,7 @@ def toggle_theme():
         st.markdown(
             """
             <style>
-            html, body, [class*="css"]  {
+            .stApp {
                 background-color: #FFFFFF !important;
                 color: #000000 !important;
             }
