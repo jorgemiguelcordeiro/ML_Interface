ac6632696a0936affb750937145b6a4635e20834
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2024-12-03 00:00:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 import streamlit as st
+import streamlit_authenticator as stauth
+
 st.set_page_config(page_title="Claim Injury Type Prediction App", layout="wide")
 # Import page functions
 from welcome_page import welcome_page
