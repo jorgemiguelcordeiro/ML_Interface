19088f6cae5eaa722e395d27125eae503471b16b
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2024-12-08 22:19:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 from welcome_page import welcome_page
 from input_page import input_page
 from output_page import output_page
-from eda_page import eda_page
+#from eda_page import eda_page
 
 st.set_page_config(page_title="Claim Injury Type Prediction App", layout="wide")
 
@@ -14,14 +14,14 @@ if 'page' not in st.session_state:
     st.session_state.page = 'welcome'
 
 # Sidebar Navigation
-st.sidebar.title("Navigation")
-page_selection = st.sidebar.radio("Go to", ('Welcome', 'EDA', 'Prediction'))
+#st.sidebar.title("Navigation")
+#page_selection = st.sidebar.radio("Go to", ('Welcome', 'EDA', 'Prediction'))
 
 # Map sidebar selection to st.session_state.page
 if page_selection == 'Welcome':
     st.session_state.page = 'welcome'
-elif page_selection == 'EDA':
-    st.session_state.page = 'eda'
+#elif page_selection == 'EDA':
+    #st.session_state.page = 'eda'
 elif page_selection == 'Prediction':
     # If you still want the 3-step approach: Welcome -> Input -> Output
     # Consider directly jumping to input or keep it as is.
@@ -35,8 +35,8 @@ elif st.session_state.page == 'input':
     input_page()
 elif st.session_state.page == 'output':
     output_page()
-elif st.session_state.page == 'eda':
-    eda_page()
+#elif st.session_state.page == 'eda':
+    #eda_page()
 
 
 
