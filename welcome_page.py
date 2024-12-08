e34674184f9a7ac65dc52c666a5499e5b880faae
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2024-12-08 22:26:54 +0000

    Update welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -71,7 +71,15 @@ def welcome_page():
 
     st.title("Welcome to the Claim Injury Type Prediction App")
 
- 
+    st.write("""
+        This application predicts the **Claim Injury Type** for claims processed by the New York Workers' Compensation Board (WCB).
+        
+        **Instructions:**
+        - Click the **Proceed** button **two times** to input claim-related data.
+        - Fill out the form with accurate information.
+        - Submit to receive a prediction of the injury type.
+    """)
+
 
 
 
