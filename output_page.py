3d321c5f3dc1b5f2a29423c975f2e8e0968c0b44
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2024-12-14 22:59:07 +0000

    Update output_page.py

--- output_page.py
+++ output_page.py
@@ -2,11 +2,42 @@
 #Importing the libraries
 import streamlit as st
 import joblib
+import zipfile, io
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer, KNNImputer
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, 
+                             f1_score)
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import time
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, VotingClassifier, GradientBoostingClassifier, StackingClassifier
+from sklearn.svm import SVC  
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LassoCV
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+
+
+import warnings
+from imblearn.over_sampling import SMOTE
+
 
 # Define your mapping dictionaries outside the function
 industry_code_description_mapping = {
